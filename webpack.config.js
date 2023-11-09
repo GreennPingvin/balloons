8c84507ad9d7cbb1e13b15d4e769ddc0b8518eef
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-11-09 17:38:29 +0300

    decompose scss code and improve webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,46 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "inline-source-map",
-  plugins: [new HtmlWebpackPlugin({ template: "public/index.html" })],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+module.exports = (env) => {
+  const { mode } = env;
+  return {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    devtool: "inline-source-map",
+    plugins: [
+      new HtmlWebpackPlugin({ template: "public/index.html" }),
+      new MiniCssExtractPlugin(),
+      new CopyPlugin({
+        patterns: [{ from: "public/icons", to: "icons" }],
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [
+            mode === "production"
+              ? MiniCssExtractPlugin.loader
+              : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, "public/img"), "node_modules"],
+    },
+  };
 };
