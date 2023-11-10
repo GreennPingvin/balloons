d2deed93aab0daab2fc4457a06b32abf704aa572
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-11-10 21:03:40 +0300

    add browser sync, progress plugin and some tweaks to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = (env) => {
   const { mode } = env;
+  const devMode = mode === "development";
+
   return {
-    mode: "development",
+    mode: mode || "development",
     entry: "./src/index.js",
     output: {
       filename: "main.js",
@@ -16,10 +20,19 @@ module.exports = (env) => {
     devtool: "inline-source-map",
     plugins: [
       new HtmlWebpackPlugin({ template: "public/index.html" }),
-      new MiniCssExtractPlugin(),
+      new MiniCssExtractPlugin({
+        filename: devMode ? "[name].css" : "[name].[contenthash].css",
+      }),
       new CopyPlugin({
         patterns: [{ from: "public/icons", to: "icons" }],
       }),
+      new BrowserSyncPlugin({
+        host: "localhost",
+        port: 3000,
+        proxy: "http://localhost:8080/",
+        open: false,
+      }),
+      new webpack.ProgressPlugin(),
     ],
     module: {
       rules: [
